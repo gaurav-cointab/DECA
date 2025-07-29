443a83cd2902962dd6848fb9f1d38d85e7b559e2
gaurav-cointab <gaurav@cointab.in>
2025-07-29 16:17:39 +0530

    Fixes to run in 2025

--- decalib/utils/renderer.py
+++ decalib/utils/renderer.py
@@ -17,6 +17,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from skimage.io import imread
+import imageio
 
 from . import util
 
@@ -24,20 +26,23 @@ from . import util
 def set_rasterizer(type='pytorch3d'):
     if type == 'pytorch3d':
         global Meshes, load_obj, rasterize_meshes
+        from pytorch3d.structures import Meshes
+        from pytorch3d.io import load_obj
+        from pytorch3d.renderer.mesh import rasterize_meshes
     elif type == 'standard':
         global standard_rasterize, load_obj
         import os
+        from .util import load_obj
         #     Use JIT Compiling Extensions
         #     ref: https://pytorch.org/tutorials/advanced/cpp_extension.html
-        from torch.utils.cpp_extension import load
+        from torch.utils.cpp_extension import load, CUDA_HOME
         curr_dir = os.path.dirname(__file__)
         standard_rasterize_cuda = \
             load(name='standard_rasterize_cuda',
                  sources=[f'{curr_dir}/rasterizer/standard_rasterize_cuda.cpp',
-                          f'{curr_dir}/rasterizer/standard_rasterize_cuda_kernel.cu']
-                 , extra_cuda_cflags=['-std=c++14', '-ccbin=$$(which gcc-7)']
-                 )  # cuda10.2 is not compatible with gcc9. Specify gcc 7
-
+                          f'{curr_dir}/rasterizer/standard_rasterize_cuda_kernel.cu'],
+                 extra_cuda_cflags=['-std=c++14', '-ccbin=$$(which gcc-7)'])  # cuda10.2 is not compatible with gcc9. Specify gcc 7
+        from standard_rasterize_cuda import standard_rasterize
     # If JIT does not work, try manually installation first
     # 1. see instruction here: pixielib/utils/rasterizer/INSTALL.md
     # 2. add this: "from .rasterizer.standard_rasterize_cuda import standard_rasterize" here
